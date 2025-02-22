90be4582e91c8e51b0371d5afa581838d397b3e6
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-02-21 17:53:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function asyncPoeCall(json) {
       }
   });
   try {
-    await window.Poe.sendUserMessage(JSON.stringify(json), { handler: id, stream: false });
+    await window.Poe.sendUserMessage("@ChatDB1 " + JSON.stringify(json), { handler: id, stream: false });
   } catch (e) {
     deferred.reject(e);
   }
